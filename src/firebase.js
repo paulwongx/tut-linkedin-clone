81a358f6e1fd1163d62869545996ea8505f465dc
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-02-28 02:03:37 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+require('dotenv').config();
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
